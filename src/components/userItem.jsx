40e5d79f9e1a5844a9204baf31cbd25fbb9728c9
andrii m <Andrii.Marusyk.KNM.2020@lpnu.ua>
2022-05-12 07:42:32 +0300

    done users list

--- src/components/userItem.jsx
+++ src/components/userItem.jsx
@@ -2,14 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Ripples from 'react-ripples';
 import '../../public/sass/index.scss';
+import { deleteUser } from '../api/user';
+import showError from '../toast';
 
 function UserItem({
-    data: {
-        firstname, lastname, country, city, address,
-    },
+    data,
+    onSelect,
+    onDelete,
 }) {
+    const {
+        id, firstname, lastname, country, city, address,
+    } = data;
+
+    const handleDelete = async () => {
+        try {
+            await deleteUser(id);
+            onDelete();
+        } catch (error) {
+            showError(error);
+        }
+    };
+
+    const handleSelect = () => onSelect(data);
+
     return (
-        <div className="row space-between advs-prev-container">
+        <Ripples onClick={handleSelect} className="row space-between advs-prev-container">
             <div className="row">
                 <img className="adv-avatar adv-prev-avatar" src="./img/avatar.png" alt="Avatar pic" />
                 <div className="advs-prev-content">
@@ -17,21 +34,24 @@ function UserItem({
                     <p className="info">{`${country} ${city} ${address}`}</p>
                 </div>
             </div>
-            <Ripples className="delete-user">
+            <Ripples onClick={handleDelete} className="delete-user">
                 <i className="fa fa-trash-o icon" />
             </Ripples>
-        </div>
+        </Ripples>
     );
 }
 
 UserItem.propTypes = {
-    data: PropTypes.objectOf({
+    data: PropTypes.shape({
+        id: PropTypes.number.isRequired,
         firstname: PropTypes.string.isRequired,
         lastname: PropTypes.string.isRequired,
         country: PropTypes.string.isRequired,
         city: PropTypes.string.isRequired,
         address: PropTypes.string.isRequired,
     }).isRequired,
+    onSelect: PropTypes.func.isRequired,
+    onDelete: PropTypes.func.isRequired,
 };
 
 export default UserItem;
--- src/screens/usersManagement.jsx
+++ src/screens/usersManagement.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../public/sass/index.scss';
+import { getUsers } from '../api/user';
 import EditUser from '../components/editUser';
 import UserItem from '../components/userItem';
+import showError from '../toast';
 
 function UsersManagement() {
+    const [users, setUsers] = useState([]);
+    const [selectedUser, setSelectedUser] = useState(null);
+
+    const loadUsers = async () => {
+        try {
+            const res = await getUsers();
+            setUsers(res.data);
+        } catch (error) {
+            showError(error);
+        }
+    };
+
+    useEffect(() => {
+        loadUsers();
+    }, []);
+
+    const handleSelect = (user) => setSelectedUser(user);
+
+    const renderUserItem = (item) => <UserItem key={item.id} data={item} onSelect={handleSelect} onDelete={loadUsers} />;
+
     return (
         <div>
             <div className="row start edit-root">
                 <div className="row advs-prev">
-                    <UserItem />
-                    <UserItem />
-                    <UserItem />
-                    <UserItem />
-                    <UserItem />
-                    <UserItem />
+                    {users.map(renderUserItem)}
                 </div>
                 <div className="profile-container admin-edit">
-                    <EditUser />
+                    {selectedUser && <EditUser />}
                 </div>
             </div>
             <div className="separator" />
