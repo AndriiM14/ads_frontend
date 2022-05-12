9b7f65d884c23e2972bfac76bafc309be194acac
andrii m <Andrii.Marusyk.KNM.2020@lpnu.ua>
2022-05-12 07:08:50 +0300

    done all api functions for users module

--- src/api/user.js
+++ src/api/user.js
@@ -1,4 +1,14 @@
 import axios from 'axios';
-import { DEFAULT_HEADERS } from './config';
+import { DEFAULT_HEADERS, formAuthHeaders } from './config';
 
 export const createUser = (data) => axios.post('/registration', data, { headers: DEFAULT_HEADERS });
+
+export const getBootstrap = () => axios.get('/user', { headers: formAuthHeaders() });
+
+export const getUsers = () => axios.get('/users', { headers: formAuthHeaders() });
+
+export const getUser = (id) => axios.get(`/users/${id}`, { headers: formAuthHeaders() });
+
+export const deleteUser = (id) => axios.delete(`/users/${id}`, { headers: formAuthHeaders() });
+
+export const changeUser = (id, data) => axios.put(`/users/${id}`, data, { headers: formAuthHeaders() });
--- src/components/userItem.jsx
+++ src/components/userItem.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Ripples from 'react-ripples';
 import '../../public/sass/index.scss';
 
-function UserItem() {
+function UserItem({
+    data: {
+        firstname, lastname, country, city, address,
+    },
+}) {
     return (
         <div className="row space-between advs-prev-container">
             <div className="row">
                 <img className="adv-avatar adv-prev-avatar" src="./img/avatar.png" alt="Avatar pic" />
                 <div className="advs-prev-content">
-                    <h5 className="title5">username</h5>
-                    <p className="info">20.04.2022 12:00</p>
+                    <h5 className="title5">{`${lastname} ${firstname}`}</h5>
+                    <p className="info">{`${country} ${city} ${address}`}</p>
                 </div>
             </div>
-            <a href="#" className="delete-user">
+            <Ripples className="delete-user">
                 <i className="fa fa-trash-o icon" />
-            </a>
+            </Ripples>
         </div>
     );
 }
+
+UserItem.propTypes = {
+    data: PropTypes.objectOf({
+        firstname: PropTypes.string.isRequired,
+        lastname: PropTypes.string.isRequired,
+        country: PropTypes.string.isRequired,
+        city: PropTypes.string.isRequired,
+        address: PropTypes.string.isRequired,
+    }).isRequired,
+};
 
 export default UserItem;
